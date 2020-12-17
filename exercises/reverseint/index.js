07a18af96a42c892cd70f4a27ba7d5a4a04964d0
Stanley Huang <iamstanleyhuang@gmail.com>
2020-12-17 12:37:10 -0800

    Finish reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,19 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+	// return n > -1
+	// 	? +n.toString().split('').reverse().join('')
+	// 	: -1 * n.toString().slice(1).split('').reverse().join('');
+
+	const reversed = n.toString().split('').reverse().join('');
+	return parseInt(reversed) * Math.sign(n);
+}
+
+console.log(reverseInt(15)); // -> 51
+console.log(reverseInt(981)); // -> 189
+console.log(reverseInt(500)); // -> 5
+console.log(reverseInt(-15)); // -> -51
+console.log(reverseInt(-90)); // -> -9
 
 module.exports = reverseInt;
