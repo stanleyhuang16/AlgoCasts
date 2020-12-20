72f22ddd86daa7ed8b763d48eda04c0dd7f9aeca
Stanley Huang <iamstanleyhuang@gmail.com>
2020-12-20 12:58:16 -0800

    Finish maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	const tally = {};
+	let max = 0;
+	for (let char of str) {
+		tally[char] = (tally[char] || 0) + 1;
+		max = Math.max(max, tally[char]);
+	}
+	for (let char in tally) {
+		if (tally[char] === max) return char;
+	}
+}
+
+console.log(maxChar('abcccccccd')); // -> "c"
+console.log(maxChar('apple 1231111')); // -> "1"
 
 module.exports = maxChar;
