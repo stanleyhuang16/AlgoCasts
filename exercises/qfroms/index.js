e12bbdc4d030a39c16cccbf378e1ae6d1f9e00ef
Stanley Huang <iamstanleyhuang@gmail.com>
2021-03-04 11:30:52 -0800

    Look at solution

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,56 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+	constructor() {
+		this.first = new Stack();
+		this.second = new Stack();
+	}
+
+	add(node) {
+		this.first.push(node);
+	}
+
+	remove() {
+		// this.first.peek = stack's peek() { return this.data[this.data.length - 1]; }
+		// while there is a last element in first array
+		while (this.first.peek()) {
+			// push last element in second array
+			this.second.push(this.first.pop());
+		}
+
+		// hold a record of the last element in second array
+		const record = this.second.pop();
+
+		// while there's a last element in second array
+		while (this.second.peek()) {
+			// push last element in first array
+			this.first.push(this.second.pop());
+		}
+
+		return record;
+	}
+
+	peek() {
+		while (this.first.peek()) {
+			this.second.push(this.first.pop());
+		}
+
+		const record = this.second.peek();
+
+		while (this.second.peek()) {
+			this.first.push(this.second.pop());
+		}
+
+		return record;
+	}
+}
+
+const q = new Queue();
+console.log(q.add(1));
+console.log(q.add(2));
+console.log(q.peek()); // returns 1
+console.log(q.remove()); // returns 1
+console.log(q.remove()); // returns 2
 
 module.exports = Queue;
