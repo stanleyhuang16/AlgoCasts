0437602ccd95b269c510499962d75f2d35329eab
Stanley Huang <iamstanleyhuang@gmail.com>
2021-03-08 13:06:04 -0800

    Look up solution

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -24,6 +24,35 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+function weave(sourceOne, sourceTwo) {
+	const queueThree = new Queue();
+
+	// while there's a last element in either queues
+	while (sourceOne.peek() || sourceTwo.peek()) {
+		console.log('sourceOne.peek(): ', sourceOne.peek());
+		console.log('sourceTwo.peek(): ', sourceTwo.peek());
+		// if last element in queue #1, UNSHIFT it to queueThree
+		if (sourceOne.peek()) queueThree.add(sourceOne.remove());
+		// if last element in queue #2, UNSHIFT it to queueThree
+		if (sourceTwo.peek()) queueThree.add(sourceTwo.remove());
+		console.log('queueThree: ', queueThree);
+	}
+
+	return queueThree;
+}
+
+const queueOne = new Queue();
+queueOne.add(1);
+queueOne.add(2);
+const queueTwo = new Queue();
+queueTwo.add('Hi');
+queueTwo.add('There');
+
+const q = weave(queueOne, queueTwo);
+console.log('q: ', q);
+console.log(q.remove()); // 1
+console.log(q.remove()); // 'Hi'
+console.log(q.remove()); // 2
+console.log(q.remove()); // 'There'
 
 module.exports = weave;
